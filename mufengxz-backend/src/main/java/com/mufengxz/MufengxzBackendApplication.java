a79b1e2fa67bc8b143043dc4f248ce66d02cb2bb
yishuai666 <1007904449@qq.com>
2023-08-31 15:26:20 +0800

    初始化导入

---
+++ mufengxz-backend/src/main/java/com/mufengxz/MufengxzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mufengxz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MufengxzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MufengxzBackendApplication.class, args);
+    }
+
+}
---
+++ mufengxz-backend/src/test/java/com/mufengxz/MufengxzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mufengxz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MufengxzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
