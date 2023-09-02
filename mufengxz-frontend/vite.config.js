0c7bf14c688cc25098b85ee959dc4959cfe617c7
yishuai666 <1007904449@qq.com>
2023-09-02 17:37:29 +0800

    后台管理系统

--- mufengxz-frontend/vite.config.js
+++ mufengxz-frontend/vite.config.js
@@ -2,15 +2,34 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueSetupExtend(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    // open: true,
+    host: 'localhost',
+    port: 80}
 })
